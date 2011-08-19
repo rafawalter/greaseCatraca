3ea8113b3d920f3625c1aae9ad7e1c6f3569a0b2
Rafael Walter <rafawalter@gmail.com>
2011-08-19 00:30:15 -0300

    calcula saldo

--- powerCatraca.js
+++ powerCatraca.js
@@ -14,27 +14,40 @@ function completa_com_zeros(valor) {
 	return texto;
 }
 
-function extrai_hora(data) {
-	return completa_com_zeros(data.getHours()) + ':' + completa_com_zeros(data.getMinutes());
+function mili_para_horas(mili) {
+	sinal = '';
+	if (mili < 0) {
+		sinal = '-';
+		mili = -mili;
+	}
+	horas = Math.floor(mili / (60*60*1000));
+	minutos = Math.floor(mili / (60*1000)) % 60;
+	umaHora = sinal + completa_com_zeros(horas) + ':' + completa_com_zeros(minutos);
+	console.log(mili + ' > ' + umaHora);
+	return umaHora;
+}
+
+function horas_para_mili(umaHora) {
+	horas = parseFloat(umaHora.substring(0,2));
+	minutos = parseFloat(umaHora.substring(3,5));
+	mili =  horas*60*60*1000 + minutos*60*1000;
+	console.log(umaHora + ' > ' + mili);
+	return mili
 }
 
 function subtrair_horas(umaHora, outraHora) {
-	return extrai_hora(new Date(data_hora(umaHora).getTime() - data_hora(outraHora).getTime()));			
-}
-
-function somar_horas(umaHora, outraHora) {
-	return extrai_hora(new Date(data_hora(umaHora).getTime() + data_hora(outraHora).getTime()));			
+	return mili_para_horas( horas_para_mili(umaHora) - horas_para_mili(outraHora));
 }
 
 function limita_horas(horas) {
-	MAX_HORAS_POR_DIA = "09:00";
+	MAX_HORAS_POR_DIA = "02:00";
 	if (horas > MAX_HORAS_POR_DIA) {
 		horas = MAX_HORAS_POR_DIA;
 	}
 	return horas;
 }
 
-function aplica_tolerancias_de_15mins(horas) {
+function aplica_tolerancia_de_15mins(horas) {
 	if (horas >= "06:45" && horas < "07:00") {
 		horas = "07:00";
 	}
@@ -42,12 +55,19 @@ function aplica_tolerancias_de_15mins(horas) {
 }
 
 function horas_trabalhadas(string_horas) {
-		var horas = "00:00";
+		var mili = 0;
 		marcacoes = string_horas.split(' ').filter(preenchido);
 		for (i = 0; i < marcacoes.length; i += 2) {
-			horas = somar_horas(horas, subtrair_horas(marcacoes[i+1], marcacoes[i]));
+			mili += horas_para_mili(marcacoes[i+1]) - horas_para_mili(marcacoes[i]);
 		}
-		horas = limita_horas(horas);
-		horas = aplica_tolerancias_de_15mins(horas);
+		horas = mili_para_horas(mili);
     return horas;
+}
+
+function saldo(string_horas) {
+	trabalhado = aplica_tolerancia_de_15mins(horas_trabalhadas(string_horas));
+	console.log(trabalhado);
+	total = subtrair_horas(trabalhado, "07:00");
+	total = limita_horas(total);
+	return total;		
 }
--- test_powerCatraca.js
+++ test_powerCatraca.js
@@ -1,5 +1,16 @@
 module('PowerCatraca');
 
+test('Trabalhei 0 horas', function() {
+    var string_horas = "09:00    09:00    ";
+    var resultado_esperado = "00:00";
+    same(horas_trabalhadas(string_horas), resultado_esperado);
+});
+
+test('Deve converter horas e milisegundos corretamente', function() {
+    var string_horas = "09:00";
+    same(mili_para_horas(horas_para_mili(string_horas)), string_horas);
+});
+
 test('Trabalhei 4 horas', function() {
     var string_horas = "09:00    13:00    ";
     var resultado_esperado = "04:00";
@@ -24,15 +35,20 @@ test('Sai para almocar', function() {
     same(horas_trabalhadas(string_horas), resultado_esperado);
 });
 
-test('Maximo de 9 horas trabalhadas por dia', function() {
+
+test('Deve subtrair horas corretamente', function() {
+    same(subtrair_horas("03:00", "02:00"), "01:00");
+});
+
+test('Saldo maximo de 2 horas por dia', function() {
     var string_horas = "09:00    20:00    ";
-    var resultado_esperado = "09:00";
-    same(horas_trabalhadas(string_horas), resultado_esperado);
+    var resultado_esperado = "02:00";
+    same(saldo(string_horas), resultado_esperado);
 });
 
 test('Tolerancia de 15 mins para saída de 07:00', function() {
     var string_horas = "10:00    13:00    14:00    17:45";
-    var resultado_esperado = "07:00";
-    same(horas_trabalhadas(string_horas), resultado_esperado);
+    var resultado_esperado = "00:00";
+    same(saldo(string_horas), resultado_esperado);
 });
 
