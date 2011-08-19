3a6c9a34bf2840f611d0b74c181518936352a1c9
Rafael Walter <rafawalter@gmail.com>
2011-08-19 18:12:46 -0300

    .

--- gm_scripts/powercatraca/powercatraca.user.js
+++ gm_scripts/powercatraca/powercatraca.user.js
@@ -1,11 +1,30 @@
 // ==UserScript==
 // @name           powerCatraca
 // @namespace      free
-// @include        file:///C:/Git/greaseCatraca/catraca.htm
-// @include        file:///Users/rafawalter/workspace/greaseCatraca/catraca.htm
-// @include        http://bndesnet/Bndesnet/servlet/CalandraRedirect?temp=2&proj=BNDESNet&pub=T&nome=frameset_topo_integracao&db=Bndesnet&url=http%3A%2F%2Frjapp02%2Faa%2Fseraa.nsf%2FQQMarcacao%3FOpenForm
-// @require        http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
-// @require        http://people.iola.dk/olau/flot/jquery.flot.js
+// @include       *QQMarcacao*
+// @include       http://rjapp02/aa/seraa.nsf/QQMarcacao?OpenForm
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js
 // ==/UserScript==
-alert("aqui1");
-$("html/body/form/table[5]/tbody/tr/td/div/font/table/tbody/tr/td[2]/p").append(" more text.");
+function data_hora(data) {
+	return data.getHours() + ":" + data.getMinutes();
+}
+
+function saldo() {
+	return "10:00";
+}
+
+function agora() {
+	return data_hora(new Date());
+}
+
+
+$("tr:last td:nth-child(2)").append(agora()).css("color", "#999");
+$("tr:last td:nth-child(4)").append(saldo()).css("color", "#00F");
+$("tr:last td:nth-child(5)").append("-"+saldo()).css("color", "#F00");
+linha_nova = $("table table tr:last").clone();
+$("table table").append(linha_nova);
+$("table table tr:last").css("color", "#AAA");
+$("table table tr:last td:nth-child(1)").html("*");
+$("table table tr:last td:nth-child(2)").html("03:30 13:00 / 06:00 15:30 / 07:00 16:30");
+$("table table tr:last td:nth-child(4)").html("*");
+$("table table tr:last td:nth-child(5)").html("*");
